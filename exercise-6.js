0104e48aba17ed6ec54b33b6ea7a4421c5fb2918
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-09-11 07:23:04 +0700

    Digit minimum perkalian faktor berhasil dijalankan

---
+++ exercise-6.js
@@ -0,0 +1,29 @@
+//Program Mencari Digit Perkalian Faktor Paling Sedikit
+function digitPerkalianMinimum(angka) {
+   var faktorBil = []
+   var faktorBil1 = []
+   var panjangFaktor = []
+  
+   /* Faktor-faktor bilangan */
+   for (var faktor = 0; faktor <= angka; faktor++) {
+      if (angka % faktor == 0) {
+         faktorBil.push(faktor)
+      }
+   }
+   
+   /* Himpunan string perkalian masing-masing faktor */
+   for (var i = 0; i < faktorBil.length / 2; i++) {
+      faktorBil1.push(faktorBil[i] + '' + faktorBil[faktorBil.length - 1 - i])
+   }
+
+   for (var j = 0; j < faktorBil1.length; j++) {
+      panjangFaktor.push(faktorBil[j].length)
+   }
+   panjangFaktor.sort()
+
+   for (var k = 0; k < panjangFaktor.length; k++) {
+      if ((panjangFaktor.length == 1) || (panjangFaktor[k] <= panjangFaktor[k + 1])) {
+         return panjangFaktor[0]
+      }
+   }
+}
