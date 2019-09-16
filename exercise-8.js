4bdba7964b04da5f5d7f970ed70ab770e2f3412b
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-09-16 14:03:43 +0700

    Program selesai dibuat

--- exercise-8.js
+++ exercise-8.js
@@ -1,6 +1,14 @@
 //Program Tukar Huruf
 function tukarBesarKecil(kalimat) {
-   var kataAlay = ''
-   for () {
+   var kalimatAlay = ''
+   for (var angka = 0; angka < kalimat.length; angka++) {
+      if (kalimat[angka] === kalimat[angka].toUpperCase()) {
+         kalimatAlay = kalimatAlay + kalimat[angka].toLowerCase()
+      } else if (kalimat[angka] === kalimat[angka].toLowerCase()) {
+         kalimatAlay = kalimatAlay + kalimat[angka].toUpperCase()
+      } else if ((kalimat[angka] === '.') || (kalimat[angka] === ',') || (kalimat[angka] === '-') || (kalimat[angka] === '?') || (kalimat[angka] === '!') || (kalimat[angka] === '...') || (kalimat[angka] === '"') || (kalimat[angka] === '(') || (kalimat[angka] === ')') || (kalimat[angka] === '[') || (kalimat[angka] === ']') || (kalimat[angka] === '/') || (kalimat[angka] === ' ')) {
+         kalimatAlay = kalimatAlay + kalimat[angka]
+      }
    }
+   return kalimatAlay
 }
